f2bc7150aa554a82ab950d261ed9ca51512a2b25
vanshikasingh202129 <singhvanshika0102@gmail.com>
2023-11-29 11:15:26 +0530

    Revert "final_update"
    This reverts commit b655ed63994f9af6fcf1e2eb10ba658ca5b240b2.

--- NavCuh/navapp/models.py
+++ NavCuh/navapp/models.py
@@ -5,7 +5,7 @@ class School(models.Model):
     school_name = models.CharField(max_length=50)
     Dean = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
-    school_description = models.TextField()
+    school_description = models.TextField(max_length=300)
     slugy = models.SlugField(unique=True, db_index = True)
 
     def __str__(self):
@@ -32,7 +32,6 @@ class Teacher(models.Model):
     department = models.ForeignKey(Department, related_name="teachers", on_delete=models.CASCADE)
     
     image = models.ImageField(upload_to="teacher", null=True)
-    resume = models.CharField(max_length=50, null=True)
     
     def __str__(self) -> str:
         return f"{self.name}"
